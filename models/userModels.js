2f70d98b2911d1649c8c570fe4c535a6af1c856f
Theus <matheusgoias@hotmail.com>
2024-08-27 17:58:48 +0100

    key for user

--- models/userModels.js
+++ models/userModels.js
@@ -4,11 +4,19 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
+  },
+  name: {
+    type: String,
     unique: true,
+  },
+  uniqueKey: {
+    type: String,
+    default: "c001k3y",
   },
 });
 
