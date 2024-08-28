232653e015d3f3b1c31456bbfdc50b9a3e79a13e
Theus <matheusgoias@hotmail.com>
2024-08-28 12:11:29 +0100

    implemented extras
    error-handling, helmet, disabled x-powered-by, and disabled extended url encode

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const cors = require("cors");
 const logger = require("morgan");
+const helmet = require("helmet");
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(helmet());
 app.use(cors());
+app.use(express.json());
 app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+app.disable("x-powered-by");
 
 app.use("/api", userRoutes);
 app.use("/api", messageRoutes);
@@ -30,6 +35,8 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+require("./error-handling")(app);
 
 // Start server
 const PORT = process.env.PORT || 3000;
