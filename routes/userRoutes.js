b7bd277306a76013a45dd2a67194c1de0472e8de
Theus <matheusgoias@hotmail.com>
2024-08-29 13:01:07 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,4 +32,14 @@ router.get("/users", async (req, res) => {
   }
 });
 
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await User.findById(id)
+    .then((data) => res.status(200).json(data))
+    .catch((err) =>
+      res.status(500).json({ message: "internal server error", error: err })
+    );
+});
+
 module.exports = router;
