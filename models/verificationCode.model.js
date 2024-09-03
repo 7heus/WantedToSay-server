5fb91537524f91fbb6626667353f0e41f000f5b7
Theus <matheusgoias@hotmail.com>
2024-09-03 22:53:34 +0100

    added expireAt for verification code

--- models/verificationCode.model.js
+++ models/verificationCode.model.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const verifyCodesSchema = new Schema({
   email: { type: String, required: true, unique: true },
   code: { type: String, required: true, maxLength: 6 },
-  expiry: { type: Date, expires: 60 },
+  expiresAt: { type: Date, expires: 60, default: Date.now },
 });
 
 const verifyModel = model("VerifyCode", verifyCodesSchema);
